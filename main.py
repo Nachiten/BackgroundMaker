3643e775e6982498d748451a62bd6857a0a7fb8c
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-05-02 16:03:04 -0300

    Aumentada velocidad de procesamiento

--- main.py
+++ main.py
@@ -2,50 +2,25 @@ from PIL import Image
 import numpy
 
 '''
-def contarColoresExistenes():
-    for unaFila in range(0, height):
-        for unaColumna in range(0, width):
-            colorActual = numpy_array[unaFila][unaColumna]
-
-            if noExisteEnArray(colorActual):
-                coloresExistentes.append(colorActual)
-
-
-def noExisteEnArray(unaTupla):
-    for unaTuplaColor in coloresExistentes:
-        if unaTupla[0] == unaTuplaColor[0] and \
-                unaTupla[1] == unaTuplaColor[1] and \
-                unaTupla[2] == unaTuplaColor[2]:
-            return False
-    return True
-'''
-
-
 def printearMatriz():
     for unaFila in range(0, height):
         for unaColumna in range(0, width):
             print(numpy_array[unaFila][unaColumna], end=" ")
         print("")
+'''
 
 
 def reemplazarColores():
     for unaFila in range(0, height):
         for unaColumna in range(0, width):
             for unNum in range(0, len(coloresAReemplazar)):
-                if estaEnUnMargen(numpy_array[unaFila][unaColumna], 0, unNum) and \
-                        estaEnUnMargen(numpy_array[unaFila][unaColumna], 1, unNum) and \
-                        estaEnUnMargen(numpy_array[unaFila][unaColumna], 2, unNum):
+                if coloresAReemplazar[unNum][0] == numpy_array[unaFila][unaColumna][0] and \
+                        coloresAReemplazar[unNum][1] == numpy_array[unaFila][unaColumna][1] and \
+                        coloresAReemplazar[unNum][2] == numpy_array[unaFila][unaColumna][2]:
                     numpy_array[unaFila][unaColumna] = coloresReemplazados[unNum]
         if unaFila % 100 == 0:
             porcentajeTerminado = unaFila * 100 / height
             print("Porcentaje terminado: " + str(porcentajeTerminado))
-
-
-def estaEnUnMargen(unaTupla, indexColor, indexArray):
-    margen = 1
-    return coloresAReemplazar[indexArray][indexColor] - margen <= \
-           unaTupla[indexColor] <= \
-           coloresAReemplazar[indexArray][indexColor] + margen
 
 
 def leerPaleta():
@@ -58,8 +33,7 @@ def leerPaleta():
 
 numeroPaleta = 0
 
-for unNumFor in range(4, 6):
-
+for unNumFor in range(5, 6):
     numeroPaleta = unNumFor
 
     image = Image.open("C:\\Users\ignac\Desktop\Imagenes\ImagenModelo.png")
